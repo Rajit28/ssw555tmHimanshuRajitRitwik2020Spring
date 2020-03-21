bef9f55d8a0e5529ec38bf9bcea3f5359fa1a283
ritvik123 <ritvik2013@gmail.com>
2020-03-21 09:15:01 -0400

    Update autoTest_sprint2.py
    Adding test cases

--- autoTest_sprint2.py
+++ autoTest_sprint2.py
@@ -36,6 +36,33 @@ class TestUS08(unittest.TestCase):
 		self.assertTrue(len(marriageError) == 0 and len(divorceError) == 0, "US08: All BIRTHS are correct and occur correctly for parents Marriage/Divorce dates!")
 
 
+# Unittest for US12
+# Author: Ritvik Tiwari
+class TestUS12(unittest.TestCase):
+	def test_US12_individuals(self):
+		ageError = sprint2.US12()
+		for person in ageError:
+            		if person[1].gender =="M":
+				print("ERROR: INDIVIDUAL: US12: " + person[1].id + " Name " + person[1].name + "is more than 80 years older than the child " + person[0].id + ": Father's birthday"+ str(person[1].birthday)+", Child's birthday " +str(person[0].birthday))
+            		else:
+                 		print("ERROR: INDIVIDUAL: US12: " + person[1].id + " Name " + person[1].name + "is more than 60 years older than the child " + person[0].id + ": Mother's birthday"+ str(person[1].birthday)+", Child's birthday " +str(person[0].birthday))
+		self.assertTrue(len(ageError) == 0, "US12: Completed")
+
+
+# Unittest for US17
+# Author: Ritvik Tiwari
+class TestUS17(unittest.TestCase):
+
+	def test_US17_individuals(self):
+		Error = sprint2.US17()
+		for person in ageError:
+            		if person[1].gender =="M":
+				print("ERROR: INDIVIDUAL: US17: " + person[0].id + " Name " + person[0].name + "is married to his child " + person[1].id)
+            		else:
+                 		print("ERROR: INDIVIDUAL: US17: " + person[0].id + " Name " + person[0].name + "is married to her child " + person[1].id)
+		self.assertTrue(len(Error) == 0, "US17: Completed")
+		
+		
 if __name__ == '__main__':
 	main_parser.outputTable('gedcomTests/sprint2_test.ged')
 	print("")
