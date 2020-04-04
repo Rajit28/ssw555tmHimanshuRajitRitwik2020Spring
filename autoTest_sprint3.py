92aded93d2c2e9e9d0c717707126f63be1a6a341
ritvik123 <ritvik2013@gmail.com>
2020-04-04 11:21:58 -0400

    US17 and 18

--- autoTest_sprint3.py
+++ autoTest_sprint3.py
@@ -27,6 +27,34 @@ class TestUS14(unittest.TestCase):
 		
 		self.assertTrue(len(siblingErrorList) == 0 , "US14: Number of children with same birthday is less than 5!")
 
+		
+# Unittest for US17
+# Author: Ritvik Tiwari
+class TestUS17(unittest.TestCase):
+
+	def test_US17_individuals(self):
+		Error = sprint2.US17()
+		for person in Error:
+            		if person[1].gender =="M":
+				print("ERROR: INDIVIDUAL: US17: " + person[0].id + " Name " + person[0].name + "is married to his child " + person[1].id)
+            		else:
+                 		print("ERROR: INDIVIDUAL: US17: " + person[0].id + " Name " + person[0].name + "is married to her child " + person[1].id)
+		self.assertTrue(len(Error) == 0, "US17: Completed")
+
+
+# Unittest for US18
+# Author: Ritvik Tiwari
+class TestUS18(unittest.TestCase):
+
+	def test_US18_individuals(self):
+		Error = sprint2.US18()
+
+		for person in Error:
+			print("ERROR: INDIVIDUAL: US18: " + person[0].id + " Name " + person[0].name + "is married to sibling " + person[1].id)
+		
+		self.assertTrue(len(Error) == 0, "US18: Completed")
+
+
 if __name__ == '__main__':
 	main_parser.outputTable('gedcomTests/sprint1_test.ged')
 	print("")
